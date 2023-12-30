a18900e090641945a0bb9165402a3b9eb051eb17
weeiwaai <weeiwaai02@gmail.com>
2023-12-30 19:58:07 +1030

    vercel

--- src/store/modules/weather/index.js
+++ src/store/modules/weather/index.js
@@ -43,7 +43,7 @@ const actions = {
     fetchWeather(context, id) {
 
         context.commit("LOADING_PENDING");
-        axios.get('/api/weather', {
+        axios.get('https://weather-app-nine-alpha-95.vercel.app/weather', {
             params: {
                 id: id
             }
